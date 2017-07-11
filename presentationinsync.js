ee7fe45fc6953e82233a9549627945c0bbadfa7c
Dion <dionvanvelde@gmail.com>
2017-07-11 18:13:25 +0200

    change line port

--- presentationinsync.js
+++ presentationinsync.js
@@ -3,9 +3,9 @@ var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
-var port = process.env.PORT || process.env.port || 3000;
 var path = require('path');
 require('dotenv').config({path: path.join(__dirname,".env")});
+var port = process.env.PORT || process.env.port || 3000;
 
 server.listen(port, function () {
 	console.log('Server listening at port http://localhost:%d', port);

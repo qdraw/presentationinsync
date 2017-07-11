4a1362ce6c798107d9d8860055f97046d629a6d3
Dion <dionvanvelde@gmail.com>
2017-07-11 15:55:31 +0200

    update code

--- presentationinsync.js
+++ presentationinsync.js
@@ -3,7 +3,9 @@ var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || process.env.port || 3000;
+var path = require('path');
+require('dotenv').config({path: path.join(__dirname,".env")});
 
 server.listen(port, function () {
 	console.log('Server listening at port http://localhost:%d', port);
@@ -12,7 +14,8 @@ server.listen(port, function () {
 
 app.use( "/images", express.static( __dirname + '/images' ) );
 
-global.imagelist = ["https://media.qdraw.nl/Bezienswaardigheden/nu_inzetten_op_de_service_by_speech_revolutie/1000/20170323_094652_kl1k.jpg","https://media.qdraw.nl/log/deventer-op-stelten-2017/1000/20170707_212524__DSC09410_e_kl1k.jpg"]
+var getdropbox = require('./lib/getdropbox.js');
+getdropbox.syncFolder();
 
 
 // Routing
@@ -33,7 +36,9 @@ var currentItem = 0;
 var allClients = [];
 
 io.on('connection', function (socket) {
-	socket.emit('status', { image: global.imagelist[currentItem] });
+	if (global.content_hash_list[currentItem] !== undefined) {
+		socket.emit('status', { image: "images/" + global.content_hash_list[currentItem] });
+	}
 
 	allClients.push(socket);
 	socket.on('disconnect', function() {
@@ -45,36 +50,34 @@ io.on('connection', function (socket) {
 
 
 	socket.on('next', function (data) {
-		io.clients((error, clients) => {
-			if (error) throw error;
-			console.log(clients); // => [6em3d4TJP8Et9EMNAAAA, G5p55dHhGgUnLUctAAAB]
-		});
 
-		console.log("next");
+		// console.log("next");
 		currentItem++
 
-		if (currentItem >= imagelist.length ) {
+		if (currentItem >= global.content_hash_list.length ) {
 			currentItem = 0
 		}
-		socket.emit('status', { image: global.imagelist[currentItem] });
-		socket.broadcast.emit('status', { image: global.imagelist[currentItem] });
+		socket.emit('status', { image: "images/" + global.content_hash_list[currentItem] });
+		socket.broadcast.emit('status', { image: "images/" + global.content_hash_list[currentItem] });
 	});
 
 	socket.on('prev', function (data) {
-		console.log("prev");
+		// console.log("prev");
 
 		currentItem--
 
 		if (currentItem <= -1 ) {
-			currentItem = imagelist.length-1
+			currentItem = global.content_hash_list.length-1
 		}
-		socket.emit('status', { image: global.imagelist[currentItem] });
-		socket.broadcast.emit('status', { image: global.imagelist[currentItem] });
+		socket.emit('status', { image: "images/" + global.content_hash_list[currentItem] });
+		socket.broadcast.emit('status', { image: "images/" + global.content_hash_list[currentItem] });
 	});
 
 });
-
-
+// 
+// setInterval
+//
+// allClients
 
 // io.on('connection', function (socket) {
 //
